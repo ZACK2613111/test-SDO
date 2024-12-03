7e34acebabe84fb96cae5c025433a5a183b50b2c
ZACK2613111 <zackbelhadj261311@gmail.com>
2024-12-03 03:15:39 +0100

    Registring users from the Frontend

--- client/src/app/authentication/register/page.tsx
+++ client/src/app/authentication/register/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { Grid, Box, Card, Typography, Stack } from "@mui/material";
-import Link from "next/link";
 import PageContainer from "@/app/(DashboardLayout)/components/container/PageContainer";
 import Logo from "@/app/(DashboardLayout)/layout/shared/logo/Logo";
 import AuthRegister from "../auth/AuthRegister";
 
-const Register2 = () => (
+const RegisterPage = () => (
   <PageContainer title="Register" description="this is Register page">
     <Box
       sx={{
@@ -22,12 +21,7 @@ const Register2 = () => (
         },
       }}
     >
-      <Grid
-        container
-        spacing={0}
-        justifyContent="center"
-        sx={{ height: "100vh" }}
-      >
+      <Grid container justifyContent="center" sx={{ height: "100vh" }}>
         <Grid
           item
           xs={12}
@@ -38,52 +32,11 @@ const Register2 = () => (
           justifyContent="center"
           alignItems="center"
         >
-          <Card
-            elevation={9}
-            sx={{ p: 4, zIndex: 1, width: "100%", maxWidth: "500px" }}
-          >
+          <Card elevation={9} sx={{ p: 4, zIndex: 1, width: "100%", maxWidth: "500px" }}>
             <Box display="flex" alignItems="center" justifyContent="center">
               <Logo />
             </Box>
-            <AuthRegister
-              subtext={
-                <Typography
-                  variant="subtitle1"
-                  textAlign="center"
-                  color="textSecondary"
-                  mb={1}
-                >
-                  Your Social Campaigns
-                </Typography>
-              }
-              subtitle={
-                <Stack
-                  direction="row"
-                  justifyContent="center"
-                  spacing={1}
-                  mt={3}
-                >
-                  <Typography
-                    color="textSecondary"
-                    variant="h6"
-                    fontWeight="400"
-                  >
-                    Already have an Account?
-                  </Typography>
-                  <Typography
-                    component={Link}
-                    href="/authentication/login"
-                    fontWeight="500"
-                    sx={{
-                      textDecoration: "none",
-                      color: "primary.main",
-                    }}
-                  >
-                    Sign In
-                  </Typography>
-                </Stack>
-              }
-            />
+            <AuthRegister />
           </Card>
         </Grid>
       </Grid>
@@ -91,4 +44,4 @@ const Register2 = () => (
   </PageContainer>
 );
 
-export default Register2;
+export default RegisterPage;
